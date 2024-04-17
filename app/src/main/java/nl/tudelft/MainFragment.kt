3b7aba33a01ef8b1c45a1d7eea0247c7c55e8928
François <francois.ezard@gmail.com>
2024-04-17 22:49:22 +0200

    hello world apk

---
+++ app/src/main/java/nl/tudelft/MainFragment.kt
@@ -0,0 +1,18 @@
+package nl.tudelft
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.example.myapplication.R
+
+class MainFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?, savedInstanceState: Bundle?): View?
+            = inflater.inflate(R.layout.main_fragment, container, false).apply {
+        val button = Button(this.context)
+        button.text = getString(R.string.button_text)
+    }
+}
