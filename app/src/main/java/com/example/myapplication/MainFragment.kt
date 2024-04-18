3c2acd8520455a791922a3cd7fe535e4d8edeaa8
Jop Schaap <59020232+JopSchaap@users.noreply.github.com>
2024-04-18 13:54:46 +0200

    add app that shows the date

--- app/src/main/java/com/example/myapplication/MainFragment.kt
+++ app/src/main/java/com/example/myapplication/MainFragment.kt
@@ -1,13 +1,23 @@
 package com.example.myapplication
 
 import android.content.Intent
+import android.icu.text.SimpleDateFormat
 import android.net.Uri
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import java.util.Calendar
+import java.util.Locale
+
+
+
 
 
 class MainFragment : Fragment() {
@@ -18,18 +28,23 @@ class MainFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val mylayout = LinearLayout(container!!.context)
+        val mylayout = RelativeLayout(container!!.context)
+        mylayout.gravity = Gravity.CENTER
         val lp = LinearLayout.LayoutParams(
             ViewGroup.LayoutParams.MATCH_PARENT,
             ViewGroup.LayoutParams.MATCH_PARENT
         )
-//        val mybutton = Button(container.context)
-//        mybutton.text = "hhhhhh"
-//        mybutton.setLayoutParams(lp)
-//        mylayout.addView(mybutton)
+        val mybutton = TextView(container.context)
 
-        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.duckduckgo.com"))
-        startActivity(browserIntent)
+        val date  = Calendar.getInstance().getTime();
+        val df = SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault())
+        mybutton.text = df.format(date)
+
+
+        mybutton.setLayoutParams(lp)
+        mybutton.gravity = Gravity.CENTER
+        mybutton.textSize = 32.0F
+        mylayout.addView(mybutton)
 
         return mylayout
     }
