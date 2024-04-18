6a9faaaa6f1c4d951fb9032abd8995b213b60080
François <francois.ezard@gmail.com>
2024-04-18 11:41:03 +0200

    Revert "trying again with .xml"
    This reverts commit f9172d9f6501b4ccf65cfc6dab068f9812dffb5d.

--- app/src/main/java/com/execmodule/helloworld/MainFragment.kt
+++ app/src/main/java/com/execmodule/helloworld/MainFragment.kt
@@ -1,39 +1,23 @@
-//package com.execmodule.helloworld
-//
-//import android.os.Bundle
-//import android.view.LayoutInflater
-//import android.view.View
-//import android.view.ViewGroup
-//import android.widget.Button
-//import android.widget.LinearLayout
-//import androidx.fragment.app.Fragment
-//
-//class MainFragment : Fragment() {
-//    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-//        val layout = LinearLayout(container?.context)
-//        val layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
-//
-//        val button = Button(container?.context)
-//        button.text = "Hello World"
-//        button.layoutParams = layoutParams
-//        layout.addView(button)
-//
-//        return layout
-//    }
-//}
-
 package com.execmodule.helloworld
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
-import com.example.myapplication.R
 
-class MainFragment: Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        activity?.setContentView(R.layout.main_fragment)
-        val button = Button(context)
-        button.text = getString(R.string.button_text)
+class MainFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val layout = LinearLayout(container?.context)
+        val layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+
+        val button = Button(container?.context)
+        button.text = "Hello World"
+        button.layoutParams = layoutParams
+        layout.addView(button)
+
+        return layout
     }
 }
