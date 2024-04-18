30e6f898e2a8cfb7320716e4c0382f471587028c
François <francois.ezard@gmail.com>
2024-04-18 18:59:45 +0200

    cleanup

---
+++ app/src/main/java/com/execmodule/random/MainFragment.kt
@@ -0,0 +1,23 @@
+package com.execmodule.random
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import androidx.fragment.app.Fragment
+class MainFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val layout = LinearLayout(container?.context)
+        val layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+
+        val button = Button(container?.context)
+        button.text = "Turn On"
+        button.layoutParams = layoutParams
+        layout.addView(button)
+
+        return layout
+    }
+}
